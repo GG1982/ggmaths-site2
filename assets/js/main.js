2b0276910e37d213beed801bad7c60d86631b210
GG1982 <giuliano.grasso26@gmail.com>
2022-12-10 22:57:15 +0000

    update to ggmaths

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Calculates height of each flex item cell in rotate-text
+
+const rotateTextFlex = document.querySelector(".rotate-text")
+const span = rotateTextFlex.querySelector("span")
+
+const observer = new ResizeObserver((entries) => {
+    const height = entries[0].contentRect.height;
+    document.documentElement.style.setProperty('--rotate-text-height', height + "px")
+    }
+)
+
+observer.observe(span)
+
+
